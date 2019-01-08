aa33cdc749047e1561de83c0f36a56e00c629833
Pradeep <pradeepcausjobs@gmail.cm>
2019-01-08 23:04:51 +1100

    Added gravatar

--- erp-app/src/app/pages/home/home.page.ts
+++ erp-app/src/app/pages/home/home.page.ts
@@ -1,19 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { PopoverController } from '@ionic/angular';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PopoverController, Platform } from '@ionic/angular';
 import { PopoverMenuComponent } from '../../shared/components/popover-menu/popover-menu.component';
 import { NavMenuItem } from '../../models/NavMenuItem';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage implements OnInit, OnDestroy {
 
-  constructor(public popoverController: PopoverController) { }
+  private subscriptions: Subscription[] = [];
+
+  constructor(public popoverController: PopoverController, private platform: Platform) {
+    console.log('Home ctor()');
+
+    this.subscriptions.push(this.platform.backButton.subscribe(() => {
+      console.log('Back button');
+      alert('Back button');
+    }));
+  }
 
   ngOnInit() {
-    const abc: NavMenuItem =  {
+    console.log('Home ngOnInit()');
+
+    const abc: NavMenuItem = {
       title: 'Home',
       url: '/home',
       icon: 'home',
@@ -22,6 +34,10 @@ export class HomePage implements OnInit {
       children: null
     };
     console.log(abc);
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach((elem) => { elem.unsubscribe(); });
   }
 
   async presentPopover(ev: any) {
--- erp-app/src/app/pages/login/login.page.ts
+++ erp-app/src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,27 +9,12 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private camera: Camera) { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
   login() {
-    console.log('clicked login()');
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    };
-    this.camera.getPicture(options).then((imageData) => {
-     // imageData is either a base64 encoded string or a file URI
-     // If it's base64 (DATA_URL):
-     let base64Image = 'data:image/jpeg;base64,' + imageData;
-     console.log('got some data');
-    }, (err) => {
-     // Handle error
-     console.log('got some error');
-    });
+    this.router.navigateByUrl('/home');
   }
 }
